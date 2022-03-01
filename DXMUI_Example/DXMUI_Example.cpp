9a2dafa64ca723050eb3076204c91e5843464f97
Felix Ulvmåne <38222220+Ulvmane@users.noreply.github.com>
2022-03-01 12:23:01 +0100

    Added example project

--- DXMUI_Example/DXMUI_Example.cpp
+++ DXMUI_Example/DXMUI_Example.cpp
@@ -1,11 +1,20 @@
 #include <iostream>
 #include "Parser\Markup\DXMParser.h"
 
+#include <chrono>
+#include <ctime>
+#include <cmath>
+
 using namespace DXMUI;
 
 int main()
 {
     DXMParser markupParser;
+    auto clock_start = std::chrono::system_clock::now();
     DXMBuilder UIBuilder = markupParser.Parse(L"Test_Markup.DXMarkup");
-    std::cout << "Hello World!\n";
+    Canvas myCanvas = UIBuilder.Build();
+    myCanvas.Render();
+    auto clock_now = std::chrono::system_clock::now();
+    auto elapsedTime = clock_now - clock_start;
+    std::cout << std::chrono::duration_cast<std::chrono::milliseconds>(clock_now - clock_start).count() << "ms";
 }
